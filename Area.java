31ef772ed26473b15bfdf12c3628eee36d12db38
sincy1999 <77265425+sincy1999@users.noreply.github.com>
2021-09-28 22:37:04 +0530

    Add files via upload

---
+++ Area.java
@@ -0,0 +1,16 @@
+package graphics;
+public class Area
+{
+public static void main(String args[])
+	{
+		Square s=new Square();
+		Rectangle r=new Rectangle();
+		Triangle t=new Triangle();
+		Circle c=new Circle();
+		System.out.println("Area of square: "+s.area(4,0));
+		System.out.println("Area of rectangle: "+r.area(2,4));
+		System.out.println("Area of triangle: "+t.area(4,8));
+		System.out.println("Area of circle: "+c.area(2,0));
+	}
+}
+   
